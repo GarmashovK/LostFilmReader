95acef19e30359f7e61db12b407e9b1609305aeb
GarmashovK <garmashov.konstantin@yandex.ru>
2014-08-17 19:05:03 +0400

    Added SerialsList and SerialItem

---
+++ LostFilmLibrary/Serials/SerialsList.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostFilmLibrary.Serials
+{
+    class SerialsList : ObservableCollection<SerialItem>
+    {
+        public Task LoadAsync()
+        {
+            var url = "http://www.lostfilm.tv/serials.php";
+            var page = await Common.GetPage(url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(page);
+
+            var mid = Common.GetMid(doc);
+        }
+    }
+}
