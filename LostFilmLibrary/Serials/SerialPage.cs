a4eb7b44b2dc99b5e764e11318b87fa997b83ccd
GarmashovK <garmashov.konstantin@yandex.ru>
2014-10-01 21:44:44 +0400

    Breakpoint

---
+++ LostFilmLibrary/Serials/SerialPage.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Threading.Tasks;
+
+namespace LostFilmLibrary.Serials
+{
+    public class SerialPage
+    {
+        public string Country { get; set; }
+        public string ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public bool Status { get; set; }
+
+        public async Task Load(string pageUrl)
+        {
+            var page = await Common.GetPage(pageUrl);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(page);
+
+
+        }
+    }
+}
---
+++ LostFilmLibraryTest/SerialsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LostFilmLibrary.Serials;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace LostFilmLibraryTest
+{
+    [TestClass]
+    public class SerialsTest
+    {
+        [TestMethod]
+        public async Task Test_SuccessfulLoadingOfSerialsList()
+        {
+            LostFilmLibrary.LFOptions.Cookies = null;
+            var serialsList = new SerialsList();
+            await serialsList.LoadAsync();
+
+            Assert.AreEqual(serialsList.Count != 0, true);
+        }
+
+        [TestMethod]
+        public async Task Test_SuccessfulSerialPageLoading()
+        {
+            LostFilmLibrary.LFOptions.Cookies = null;
+
+        }
+    }
+}
